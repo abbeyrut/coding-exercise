5a35371bf7413772e178d0a231d88bdb41f6ee0d
Abbey Rut <abbey.rut@gmail.com>
2022-10-28 22:54:11 +0300

    Define JS function bingo

--- exercise.js
+++ exercise.js
@@ -1,5 +1,31 @@
 
+
 function myShoes(shoes) {
 return 'running shoes';
 }
-console.log(myShoes);
+console.log(myShoes());
+
+
+
+function myList(num) {
+let result;
+if (num > 5) {
+    result = "Bingo";
+}
+else if(num === 1)
+{
+result = "Yes";
+}
+else if(num >= 6)
+{
+result = false;
+}
+    else {
+    result = "Way to go";
+}
+return result;
+}
+
+console.log(myList(5))
+
+
